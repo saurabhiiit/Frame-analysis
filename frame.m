b7cf5b1ae455d70795bf3c5c390a9fe6c7ca749f
saurabh <saurabh.agrahari@research.iiit.ac.in>
2016-09-19 20:48:48 +0530

    Added BMD & SFD features

--- frame.m
+++ frame.m
@@ -114,7 +114,7 @@ Rab = abs(Rlb);
      flag= 0 ;
      flag2 =0;
      flag3=0;
-     for x = 0:0.1:lb
+     for x = 0:0.02:lb
          if(cnt1<=npb && x==pbx(cnt1) && flag==0)
              v = [v v(end)-pbm(cnt1) ];
              cnt1 = cnt1 +1;
@@ -144,16 +144,23 @@ Rab = abs(Rlb);
          if(x==lb)
              v = [v v(end)+Rbb];
          end
-         disp(x);
-         disp(v);
+         %disp(x);
+         %disp(v);
          if(flag3==0)
             v = [v v(end)];
          end
          xi = x;
      end
-     
-     x= 0:0.1:lb+0.2;
-     plot(x,v);
+     v2=[];
+     for x= 0:0.02:lb+0.02
+         v2=[v2 0]; 
+     end
+     v1 = v(1:end-1);
+     x= 0:0.02:lb+0.02;
+     plot(x,v1,x,v2,'linewidth',2);
+     xlabel('Length of the beam in m)');
+     ylabel('Shear Force in KN');
+     title('Shear force diagram');
      
  %% Shear force vector for left coulumn
      vl = [Rlax];
@@ -254,59 +261,9 @@ Rab = abs(Rlb);
      plot(x,vr);
      
 
-     
-%% Bending moment of beam
-j = 0;
-for x=0:1:lb
-    j = j + 1;
-    temp = 0;
-   % temp1 = 0;
-    for pi = 1:npb
-      if(x<pbx(pi))
-    %    v1(j) = 0;
-        m1(j)= 0;
-        break;
-      else
-     %     temp1 = temp1 + pbm(pi);
-        temp = temp + (pbm(pi)*(x-pbx(pi)));
-      %  v1(j) = 0;
-        m1(j) = 0 ;
-      end
-    end
-    for ui = 1:nub
-      if(x<ubx(ui,1))
-        m1(j)= 0;   
-       % v1(j) = 0;
-        break;
-      elseif(x>=ubx(ui,1) && x<ubx(ui,2))
-        temp = temp + ((ubm(ui)*((x-ubx(ui,1))^2))/2);
-        %temp1 = temp1 + ubm(ui)*(x-ubx(ui,1));
-        %v1(j) = 0;
-        m1(j) = 0 ;
-      elseif(x>=ubx(ui,2))
-          temp = temp + (ubm(ui)*(ubx(ui,2) - ubx(ui,1))*(x-((ubx(ui,2) + ubx(ui,1))/2)));
-          %temp = temp + ubm(ui)*(ubx(ui,2) - ubx(ui,1));
-          %disp(ubm(ui)*(ubx(ui,2) - ubx(ui,1))*(x-((ubx(ui,2) + ubx(ui,1))/2)));
-          %disp(temp);
-         % v1(j) = 0;
-          m1(j) = 0 ;
-      end
-    end
-  %  if x<lb
-   %  v(j) = Rab - temp1;
-    %else
-    % v(j) = Rab + Rbb -temp1;
-   % end
-     m(j) = abs(Mlb)+(x*Rab) -temp;
-end
-x=0:1:lb;
-%plot(v,m,x,v1,'linewidth',2);
-%hold on
-plot(x,m,x,m1,'linewidth',2);
-
 %% shear force
 j = 0;
-for x=0:1:lb
+for x=0:0.02:lb
     j = j + 1;
     temp1 = 0;
     for pi = 1:npb
@@ -315,26 +272,18 @@ for x=0:1:lb
         break;
       else
           temp1 = temp1 + pbm(pi);
-      %  temp = temp + (pbm(pi)*(x-pbx(pi)));
         v1(j) = 0;
         m1(j) = 0 ;
       end
     end
     for ui = 1:nub
       if(x<ubx(ui,1))
-    %     m1(j)= 0;   
         v1(j) = 0;
         break;
       elseif(x>=ubx(ui,1) && x<ubx(ui,2))
-        %temp = temp + ((ubm(ui)*((x-ubx(ui,1))^2))/2);
         temp1 = temp1 + ubm(ui)*(x-ubx(ui,1));
         v1(j) = 0;
-        %m1(j) = 0 ;
       elseif(x>=ubx(ui,2))
-          %temp = temp + (ubm(ui)*(ubx(ui,2) - ubx(ui,1))*(x-((ubx(ui,2) + ubx(ui,1))/2)));
-          %temp = temp + ubm(ui)*(ubx(ui,2) - ubx(ui,1));
-          %disp(ubm(ui)*(ubx(ui,2) - ubx(ui,1))*(x-((ubx(ui,2) + ubx(ui,1))/2)));
-          %disp(temp);
           temp1 = temp1 + ubm(ui)*(ubx(ui,2) - ubx(ui,1));
           v1(j) = 0;
           m1(j) = 0 ;
@@ -346,7 +295,44 @@ for x=0:1:lb
      v(j) = Rab + Rbb -temp1;
     end
 end
-x=0:1:lb;
+x=0:0.02:lb;
+subplot(3,1,1)
 plot(x,v,x,v1,'linewidth',2);
-        
-        
+title('shear force');
+xlabel('length of the beam in mm');
+ylabel('shear force in n');  
+     
+%% Bending moment of beam
+j = 0;
+for x=0:0.02:lb
+    j = j + 1;
+    temp = 0;
+    for pi = 1:npb
+      if(x<pbx(pi))
+        m1(j)= 0;
+        break;
+      else
+        temp = temp + (pbm(pi)*(x-pbx(pi)));
+        m1(j) = 0 ;
+      end
+    end
+    for ui = 1:nub
+      if(x<ubx(ui,1))
+        m1(j)= 0;   
+        break;
+      elseif(x>=ubx(ui,1) && x<ubx(ui,2))
+        temp = temp + ((ubm(ui)*((x-ubx(ui,1))^2))/2);
+        m1(j) = 0 ;
+      elseif(x>=ubx(ui,2))
+          temp = temp + (ubm(ui)*(ubx(ui,2) - ubx(ui,1))*(x-((ubx(ui,2) + ubx(ui,1))/2)));
+          m1(j) = 0 ;
+      end
+    end
+    m(j) = abs(Mlb)+(x*Rab) -temp;
+end
+x=0:0.02:lb;
+subplot(3,1,2)
+plot(x,m,x,m1,'linewidth',2);
+title('bending moment');
+xlabel('length of the beam in mm');
+ylabel('bending moment in n-mm');
